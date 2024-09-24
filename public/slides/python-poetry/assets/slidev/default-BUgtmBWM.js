b7d9f291b3eacd8bc89be1fbc717cf98c6932c03
patrick <patrickwuke@163.com>
2024-09-24 18:19:40 +0800

    fix errors

--- public/slides/python-poetry/assets/slidev/default-BUgtmBWM.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-BN5Lu5kq.js";import{_ as r}from"../index-CeL4nCPK.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
